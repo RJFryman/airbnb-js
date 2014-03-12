0d2f80683505354b3ef8bd39936af4c6621559cb
Robert Fryman <robert.fryman@gmail.com>
2014-03-12 12:13:05 -0500

    regs unit

---
+++ app/models/user.js
@@ -0,0 +1,49 @@
+'use strict';
+
+var bcrypt = require('bcrypt');
+var users = global.nss.db.collection('users');
+var email = require('../lib/email');
+//var email = require('../lib/email').sendWelcome;
+
+module.exports = User;
+
+function User(data){
+  this.role = data.role;
+  this.email = data.email;
+  this.password = data.password;
+}
+
+User.prototype.register = function(fn){
+  var self = this;
+  hashPassword(self.password, function(hashedPwd){
+    self.password = hashedPwd;
+    insert(self, function(err){
+      if(self._id){
+        email.sendWelcome({to:self.email}, function(err, body){
+          fn(err, body);
+        });
+      }else{
+        fn();
+      }
+    });
+  });
+};
+
+function insert(user, fn){
+  users.findOne({email:user.email}, function(err, userFound){
+    if(!userFound){
+      users.insert(user, function(err, record){
+        fn(err);
+      });
+    }else{
+      fn();
+    }
+  });
+}
+
+function hashPassword(password, fn){
+  bcrypt.hash(password, 8, function(err, hash){
+    fn(hash);
+  });
+}
+
